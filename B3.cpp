3d6b9cf865d0b5c4745860a4d49ae0fb0172dd57
Dinhlu24 <dinhlunhut@gmail.com>
2024-12-20 22:05:43 +0700

    Update B3.cpp

--- B3.cpp
+++ B3.cpp
@@ -1,25 +1,33 @@
 #include <stdio.h>
-
+#define maxSize 100 //Kich thuoc toi da cua mang
 int main(){
-	int a[100]={0};
-	int currentLength;
-	printf("Moi nhap vao so phan tu muon nhap: ");	scanf("%d",&currentLength);
-	for(int i=0;i<currentLength;i++){
-		printf("Moi nhap vao phan tu thu %d: ",i+1);
-		scanf("%d",a + i);
+	int arr[maxSize]={0};
+	int currentLength=0;
+	printf("Moi ban nhap vao so phan tu ban muon nhap (1 -> 100): ");	scanf("%d",&currentLength);
+	//Nhap vao so phan tu mang co cho den khi gia tri nam trong khoang (1 -> 100)
+	while(currentLength <= 0||currentLength > maxSize){
+		printf("Du lieu ban nhap vao khong hop le vui long nhap lai (1 -> 100): ");	scanf("%d", &currentLength);
 	}
 	
-	int deleteIndex;
-	printf("Moi ban nhap vao vi tri phan tu ban muon xoa: ");	scanf("%d",&deleteIndex);
-	if(deleteIndex > 0 && deleteIndex <= 100){
-		currentLength--;
-		for(int i=deleteIndex-1;i<currentLength;i++){
-			int tmp=a[i+1];
-			a[i]=tmp;
-		}
-		printf("Mang moi sau khi da xoa di phan tu vi tri thu %d: ",deleteIndex);
-		for(int i=0;i<currentLength;i++){
-			printf("%d ",a[i]);
-		}
+	//Nhap gia tri vao trong mang
+	for(int i=0;i<currentLength;i++){
+		printf("Nhap gia tri Array[%d]: ",i+1);
+		scanf("%d",&arr[i]);
+	}
+	
+	int deleteIndex=0;
+	printf("Moi ban nhap vao vi tri ban can xoa: ");	scanf("%d",&deleteIndex);
+	while(deleteIndex <= 0 || deleteIndex > currentLength){
+		printf("Gia vi tri %d khong ton tai vui long nhap lai: ",deleteIndex);	scanf("%d",&deleteIndex);
+	}
+	
+	currentLength--;
+	for(int i=deleteIndex-1;i<currentLength;i++){
+		arr[i]=arr[i+1];
+	}
+	printf("Gia tri da duoc xoa xong\n");
+	printf("Mang cua ban sau khi xoa phan tu vi tri %d:\n",deleteIndex);
+	for(int i=0;i<currentLength;i++){
+		printf("%d ",arr[i]);
 	}
 }
